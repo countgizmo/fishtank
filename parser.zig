6c18a2765191924b80faed926ae6648bce8a3a84
countgizmo <count.gizmo@gmail.com>
2025-01-04 10:47:34 +0200

    Write out Expression struct and ExpressionKind enum
    A better data structure to represent AST.

--- parser.zig
+++ parser.zig
@@ -1,20 +1,29 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 const token = @import("token.zig");
 const Token = token.Token;
 
-const Position = struct {
-    line: usize,
-    column: usize
+//TODO(evgheni): Add maps, sets, vectors
+const ExpressionKind = enum {
+    Module, // File/Namespace, kinda like a list of expressions but with metadata
+    List,
+    Symbol,
+    Literal
 };
 
+pub const Expression = struct {
+    kind: ExpressionKind,
 
-const Kind = enum {
-    Token,
-    List
-};
+    value: union(enum) {
+        module: ArrayList(Expression),
+        list: ArrayList(Expression),
+        symbol: []const u8,
+        literal: Token,
+    },
 
-const ASTNode = struct {
-    kind: Kind,
-    token: ?Token,
-    children: ?[]ASTNode,
-    position: ?Position,
+    position: struct {
+        line: usize,
+        column: usize,
+    },
 };
