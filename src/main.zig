cac06c167f023e9fbb7e2912d9967fca7b3559c3
countgizmo <count.gizmo@gmail.com>
2025-02-01 09:19:00 +0100

    Render a window

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
+const rl = @cImport({
+    @cInclude("raylib.h");
+});
+
 
 pub fn main() void {
     std.debug.print("Hello, world\n", .{});
+    rl.InitWindow(800, 600, "Fishtank");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        rl.ClearBackground(rl.PINK);
+        rl.DrawText("Hello, Fishtank", 190, 200, 20, rl.BLUE);
+        rl.EndDrawing();
+    }
 }
