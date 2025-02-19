d18e82ce83291ec58f86858bc2a4ceb41e378100
countgizmo <count.gizmo@gmail.com>
2025-02-19 17:42:02 +0100

    Load the font

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,6 @@ const Render = @import("render.zig");
 pub fn main() !void {
     rl.InitWindow(800, 600, "Fishtank");
     defer rl.CloseWindow();
-
     rl.SetTargetFPS(60);
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -21,6 +20,22 @@ pub fn main() !void {
         const status = gpa.deinit();
         if (status != .ok) @panic("Memory leak detected!");
     }
+
+    const exe_path = try std.fs.selfExeDirPathAlloc(gpa.allocator());
+    defer gpa.allocator().free(exe_path);
+
+    const font_path = "../resources/fonts/IosevkaFixed-Regular.ttf";
+    var paths = [_][]const u8 { exe_path, font_path };
+    const full_font_path = try std.fs.path.join(gpa.allocator(), &paths);
+    defer gpa.allocator().free(full_font_path);
+
+    var path_buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const absolute_font_path = try std.fs.realpath(full_font_path, &path_buf);
+    var font = rl.LoadFont(absolute_font_path.ptr);
+
+    rl.GenTextureMipmaps(&font.texture);
+    rl.SetTextureFilter(font.texture, rl.TEXTURE_FILTER_BILINEAR);
+
 
     var lexer = Lexer.init(gpa.allocator(), "(ns my-namespace (:require [clojure.string :as str]))");
     const tokens = try lexer.getTokens();
