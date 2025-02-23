2dfbe7373a8047022fcf4b23e8864ad09b46b0e7
countgizmo <count.gizmo@gmail.com>
2025-02-23 11:19:13 +0100

    Refactor font path getting into a separate function

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,21 @@ const TokenWithPosition = token_module.TokenWithPosition;
 const Parser = @import("parser.zig").Parser;
 const Lexer = @import("lexer.zig").Lexer;
 const Render = @import("render.zig");
+const Allocator = std.mem.Allocator;
 
+fn getFontPath(allocator: Allocator) ![:0]u8 {
+    const exe_path = try std.fs.selfExeDirPathAlloc(allocator);
+    defer allocator.free(exe_path);
+
+    const font_path = "../resources/fonts/IosevkaFixed-Regular.ttf";
+    var paths = [_][]const u8 { exe_path, font_path };
+    const full_font_path = try std.fs.path.join(allocator, &paths);
+    defer allocator.free(full_font_path);
+
+    var path_buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const path = try std.fs.realpath(full_font_path, &path_buf);
+    return allocator.dupeZ(u8, path);
+}
 
 pub fn main() !void {
     rl.InitWindow(800, 600, "Fishtank");
@@ -21,31 +35,22 @@ pub fn main() !void {
         if (status != .ok) @panic("Memory leak detected!");
     }
 
-    const exe_path = try std.fs.selfExeDirPathAlloc(gpa.allocator());
-    defer gpa.allocator().free(exe_path);
-
-    const font_path = "../resources/fonts/IosevkaFixed-Regular.ttf";
-    var paths = [_][]const u8 { exe_path, font_path };
-    const full_font_path = try std.fs.path.join(gpa.allocator(), &paths);
-    defer gpa.allocator().free(full_font_path);
-
-    var path_buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
-    const absolute_font_path = try std.fs.realpath(full_font_path, &path_buf);
-    var font = rl.LoadFont(absolute_font_path.ptr);
+    const font_path = try getFontPath(gpa.allocator());
+    defer gpa.allocator().free(font_path);
+    var font = rl.LoadFont(font_path.ptr);
+    defer rl.UnloadFont(font);
 
     rl.GenTextureMipmaps(&font.texture);
     rl.SetTextureFilter(font.texture, rl.TEXTURE_FILTER_BILINEAR);
 
-
-    var lexer = Lexer.init(gpa.allocator(), "(ns my-namespace (:require [clojure.string :as str]))");
+    var lexer = Lexer.init(gpa.allocator(),
+        "(ns my-namespace (:require [clojure.string :as str] [reaget.core]))");
     const tokens = try lexer.getTokens();
     defer tokens.deinit();
-
 
     var parser = Parser.init(gpa.allocator(), tokens.items);
     var module = try parser.parse("test_file.clj");
     defer module.deinit();
-
 
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
